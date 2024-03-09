b82e7818dbd9cd916822b8b7cf06c8a98866fe94
sakai <sakai263016@gmail.com>
2024-03-09 17:19:19 +0800

    2024年03月09日 17:17:29 添加了一些关于Knife4j的注解，帮助生成文档

--- tmall-admin/tmall-admin-content/src/main/java/top/sakai/tmall/admin/content/controller/CategoryController.java
+++ tmall-admin/tmall-admin-content/src/main/java/top/sakai/tmall/admin/content/controller/CategoryController.java
@@ -1,18 +1,25 @@
 package top.sakai.tmall.admin.content.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Sakai
  */
+@Api(tags = "管理段分类接口")
 @RestController
+@RequestMapping("/category/")
 public class CategoryController {
     /**
      * 添加分类接口
      *
      * @param o 分类详情
      */
+    @ApiOperation("增加类别")
+    @PostMapping("add")
     public void addCategory(Object o) {
 
     }
