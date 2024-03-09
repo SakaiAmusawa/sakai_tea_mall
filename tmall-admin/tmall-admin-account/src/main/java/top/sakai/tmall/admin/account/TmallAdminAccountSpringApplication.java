24cbb04e8d37ad7bead4d31cfa2c103f30fbceb0
sakai <sakai263016@gmail.com>
2024-03-09 12:00:25 +0800

    2024年03月09日 11:59:44 添加springboot启动类

---
+++ tmall-admin/tmall-admin-account/src/main/java/top/sakai/tmall/admin/account/TmallAdminAccountSpringApplication.java
@@ -0,0 +1,14 @@
+package top.sakai.tmall.admin.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sakai
+ */
+@SpringBootApplication
+public class TmallAdminAccountSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TmallAdminAccountSpringApplication.class, args);
+    }
+}
