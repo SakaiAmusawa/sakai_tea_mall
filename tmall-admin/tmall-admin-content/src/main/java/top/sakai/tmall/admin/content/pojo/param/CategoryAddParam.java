4b6c053ab69fe4a67165986ba5224b3d17052c44
sakai <sakai263016@gmail.com>
2024-03-11 10:46:54 +0800

    2024年03月11日 10:46:38 validation注解的复习2-长度相关

--- tmall-admin/tmall-admin-content/src/main/java/top/sakai/tmall/admin/content/pojo/param/CategoryAddParam.java
+++ tmall-admin/tmall-admin-content/src/main/java/top/sakai/tmall/admin/content/pojo/param/CategoryAddParam.java
@@ -4,10 +4,12 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * @author Sakai
@@ -19,6 +21,8 @@ public class CategoryAddParam {
     @NotNull(message = "类别名称不能为空-byNoyNull") //不能为null
     @NotBlank(message = "类别名称不能为空-byNotBlank") //不能为“ ” 、“” 、null
     @NotEmpty(message = "类别名称不能为空-byEmpty") //放到容器上
+    @Size(min = 1, max = 5, message = "类别名不能过长-bySize")
+    @Length(min = 1, max = 5, message = "类别名不能过长-byLength")
     @ApiModelProperty(required = true, dataType = "String", example = "经验")
     String name;
 }
