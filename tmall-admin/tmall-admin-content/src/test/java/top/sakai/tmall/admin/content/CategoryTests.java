f10210b6d9806d522b7772813e159c04a6407c71
sakai <sakai263016@gmail.com>
2024-03-11 17:29:09 +0800

    测试

--- tmall-admin/tmall-admin-content/src/test/java/top/sakai/tmall/admin/content/CategoryTests.java
+++ tmall-admin/tmall-admin-content/src/test/java/top/sakai/tmall/admin/content/CategoryTests.java
@@ -1,14 +1,25 @@
 package top.sakai.tmall.admin.content;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import top.sakai.tmall.admin.content.dao.CategoryMapper;
+import top.sakai.tmall.admin.content.pojo.po.CategoryPO;
 
+import javax.sql.DataSource;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.sql.SQLException;
 
 @SpringBootTest
 public class CategoryTests {
+    @Autowired
+    private CategoryMapper categoryMapper;
+    @Autowired
+    private DataSource dataSource;
+
     public static void main(String[] args) throws Exception {
         Class<?> aClass = Class.forName("top.sakai.tmall.admin.content.pojo.po.CategoryPO");
 
@@ -33,5 +44,16 @@ public class CategoryTests {
         Method wirteMethod = aClass.getMethod("setName", String.class);
         wirteMethod.invoke(o, "经验");
         System.out.println(o);
+    }
+
+    @Test
+    public void testDataSource() throws SQLException {
+        System.out.println(dataSource.getConnection());
+    }
+
+    @Test
+    public void testCategoryPO() {
+        CategoryPO categoryPO = new CategoryPO();
+        System.out.println(categoryPO);
     }
 }
