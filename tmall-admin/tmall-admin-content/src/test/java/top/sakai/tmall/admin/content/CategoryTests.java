0ebf5be67d8cb3c5603cf89b8bc67094ebd76c95
sakai <sakai263016@gmail.com>
2024-03-11 15:10:58 +0800

    2024年03月11日 15:10:16 反射机制的小小回顾

---
+++ tmall-admin/tmall-admin-content/src/test/java/top/sakai/tmall/admin/content/CategoryTests.java
@@ -0,0 +1,34 @@
+package top.sakai.tmall.admin.content;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+@SpringBootTest
+public class CategoryTests {
+    public static void main(String[] args) throws Exception {
+        Class<?> aClass = Class.forName("top.sakai.tmall.admin.content.pojo.po.CategoryPO");
+
+        for (Annotation annotation : aClass.getAnnotations()) {
+            System.out.println(annotation);
+        }
+
+        for (Constructor<?> constructor : aClass.getConstructors()) {
+            System.out.println(constructor);
+        }
+
+        for (Method declaredMethod : aClass.getDeclaredMethods()) {
+            System.out.println(declaredMethod.getName());
+        }
+        for (Field field : aClass.getDeclaredFields()) {
+            System.out.println(field.getName());
+        }
+
+        //通过反射机制创建对象
+        Object o = aClass.newInstance();
+        System.out.println(o);
+    }
+}
