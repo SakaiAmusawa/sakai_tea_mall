97820794918699a1a116812867d3ab39906b45ef
sakai <sakai263016@gmail.com>
2024-03-11 10:36:27 +0800

    2024年03月11日 10:36:12 validation注解的复习

--- tmall-admin/tmall-admin-content/src/main/java/top/sakai/tmall/admin/content/controller/CategoryController.java
+++ tmall-admin/tmall-admin-content/src/main/java/top/sakai/tmall/admin/content/controller/CategoryController.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,27 +13,25 @@ import top.sakai.tmall.admin.content.pojo.param.CategoryAddParam;
 import top.sakai.tmall.admin.content.service.ICategoryService;
 
 /**
- * 管理端分类管理相关接口
+ * @author Sakai
  */
+@Api(tags = "管理端分类接口")
 @Slf4j
-@Api(tags = "管理端类别相关接口")
 @RestController
-@RequestMapping("/category")
+@RequestMapping("/category/")
 public class CategoryController {
 
     @Autowired
     private ICategoryService categoryService;
 
     /**
-     * 增加分类
+     * 添加分类接口
      *
      * @param categoryAddParam 分类详情
      */
     @ApiOperation("增加类别")
-    @PostMapping("/add")
-    public void addCategory(@RequestBody CategoryAddParam categoryAddParam) {
-
+    @PostMapping("add")
+    public void addCategory(@Validated @RequestBody CategoryAddParam categoryAddParam) {
         categoryService.addCategory(categoryAddParam);
-
     }
 }
--- tmall-admin/tmall-admin-content/src/main/java/top/sakai/tmall/admin/content/pojo/param/CategoryAddParam.java
+++ tmall-admin/tmall-admin-content/src/main/java/top/sakai/tmall/admin/content/pojo/param/CategoryAddParam.java
@@ -5,6 +5,10 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 /**
  * @author Sakai
  */
@@ -12,6 +16,9 @@ import lombok.experimental.FieldDefaults;
 //设置字段的默认级别
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CategoryAddParam {
+    @NotNull(message = "类别名称不能为空-byNoyNull") //不能为null
+    @NotBlank(message = "类别名称不能为空-byNotBlank") //不能为“ ” 、“” 、null
+    @NotEmpty(message = "类别名称不能为空-byEmpty") //放到容器上
     @ApiModelProperty(required = true, dataType = "String", example = "经验")
     String name;
 }
