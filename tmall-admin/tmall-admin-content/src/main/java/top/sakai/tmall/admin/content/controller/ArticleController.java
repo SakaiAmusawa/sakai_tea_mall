b0b50bc3b2b6ad047ecd32d53550d4c8916977fc
sakai <sakai263016@gmail.com>
2024-03-11 14:06:36 +0800

    修改文章管理接口

--- tmall-admin/tmall-admin-content/src/main/java/top/sakai/tmall/admin/content/controller/ArticleController.java
+++ tmall-admin/tmall-admin-content/src/main/java/top/sakai/tmall/admin/content/controller/ArticleController.java
@@ -1,20 +1,27 @@
 package top.sakai.tmall.admin.content.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.sakai.tmall.common.response.JsonResult;
 
 /**
  * @author Sakai
  */
-@Api(tags = "管理端添加文章功能")
+@Api(tags = "文章管理相关接口")
 @RestController
+@RequestMapping("article/")
 public class ArticleController {
     /**
      * 添加文章接口
      *
      * @param o 文章详情
      */
-    public void addArticle(Object o) {
-
+    @ApiOperation("增加文章")
+    @PostMapping("add")
+    public JsonResult addArticle(Object o) {
+        return JsonResult.ok();
     }
 }
