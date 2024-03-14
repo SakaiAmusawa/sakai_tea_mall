a221ecc7fce56b83444db6007cf8fc143e063a64
sakai <sakai263016@gmail.com>
2024-03-14 14:47:14 +0800

    小修改

--- tmall-admin/tmall-admin-content/src/main/java/top/sakai/tmall/admin/content/service/IArticleService.java
+++ tmall-admin/tmall-admin-content/src/main/java/top/sakai/tmall/admin/content/service/IArticleService.java
@@ -5,6 +5,7 @@ import top.sakai.tmall.admin.content.pojo.param.ArticleAddParam;
 
 public interface IArticleService {
 
-    @Transactional //推荐加在service的接口方法上，可以明确的告诉使用者，这个方法支持事务，可以放心的使用
+    @Transactional
+        //推荐加在service的接口方法上，可以明确的告诉使用者，这个方法支持事务，可以放心的使用
     void save(ArticleAddParam articleAddParam);
 }
