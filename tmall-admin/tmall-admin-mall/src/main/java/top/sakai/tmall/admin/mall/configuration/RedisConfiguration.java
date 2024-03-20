a1af7f55ecd6d07be440b6e21885fd36bdb6ede5
sakai <sakai263016@gmail.com>
2024-03-20 11:10:10 +0800

    2024年03月20日 11:09:18 通过connection和redisTemplate操作数据的区别体验序列化的意义

---
+++ tmall-admin/tmall-admin-mall/src/main/java/top/sakai/tmall/admin/mall/configuration/RedisConfiguration.java
@@ -0,0 +1,28 @@
+package top.sakai.tmall.admin.mall.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.io.Serializable;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        //演示Redis的设置
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        //设置连接工厂  new 一个RedisTemplate必须设置ConnectionFactory
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //设置序列化器
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setValueSerializer(RedisSerializer.json());
+        redisTemplate.setHashKeySerializer(RedisSerializer.json());
+        redisTemplate.setHashValueSerializer(RedisSerializer.json()); // todo 为何设置Hash
+        return redisTemplate;
+    }
+
+}
--- tmall-admin/tmall-admin-mall/src/test/java/top/sakai/tmall/admin/mall/redis/RedisTests.java
+++ tmall-admin/tmall-admin-mall/src/test/java/top/sakai/tmall/admin/mall/redis/RedisTests.java
@@ -3,12 +3,16 @@ package top.sakai.tmall.admin.mall.redis;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
 
 /**
- * Redis的使用演示
+ * Redis的使用演示 配合 RedisConfiguration使用！！！
  */
 @SpringBootTest
 public class RedisTests {
@@ -18,9 +22,39 @@ public class RedisTests {
 
     @Test
     public void testRedis() {
-        Boolean k1 = redisTemplate.delete("k1");
-        System.out.println(k1);
 
+        RedisConnectionFactory connectionFactory = redisTemplate.getConnectionFactory();
+        System.out.println(connectionFactory);
+        //使用Connection连接
+        RedisConnection connection = connectionFactory.getConnection();
+        System.out.println(connection);
+        String k1 = "k1";
+        String v1 = "v1";
+        byte[] k1Bytes = k1.getBytes(StandardCharsets.UTF_8);
+        byte[] v1Bytes = v1.getBytes(StandardCharsets.UTF_8);
+        System.out.println(k1Bytes);
+        System.out.println(v1Bytes);
+        System.out.println(connection.set(k1Bytes, v1Bytes));
+        byte[] bytes = connection.get(k1Bytes);
+        String s1 = new String(bytes);
+        System.out.println(s1);
+
+        //Boolean k = redisTemplate.delete("k1");
+        //System.out.println(k);
+
+    }
+
+    @Test
+    public void testRedisTemplate() {
+        ValueOperations<String, Serializable> opsForValue = redisTemplate.opsForValue();
+        //System.out.println(opsForValue.get("k1")); //该代码无法运行，详情了解序列化器
+
+        //调用了redisTemplate中的序列化器，转换为了JSON格式
+        opsForValue.set("k2", "v2");
+        System.out.println(opsForValue.get("k2"));
+        opsForValue.set("k2", "v2-new");
+        System.out.println(opsForValue.get("k2"));
+        redisTemplate.delete("k2");
     }
 
 }
